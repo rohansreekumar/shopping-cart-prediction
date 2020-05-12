7aaa67f24ae7d2f4ba5a041403c1b1f251a7c6ee
Rohan Sreekumar <rohansreekumar@gmail.com>
2020-05-13 00:41:55 +1000

    configurations fixed, dataset increased to 22000, readme updated

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # shopping-cart-prediction
 predict the shopping cart of a user using associative rule mining (Apriori algorithm)
+
+- import the libraries - pandas, apyori, pymongo
+- run mongodb on port 27017
+- add the csv file to mongodb using the command : mongoimport --type csv -d test -c orders --headerline --drop store_transactions.csv 
+- the csv file has 22000 dummy orders from users with id between 1-50
+- run the python file
+
+Input :
+- user id of the person
+
+Output :
+- the output will be the top most frequently bought together items by the user (provided the user has more than 50 orders and bought the frequent items atleast 10 times)
+- if no frequent set can be found from the users purchase history, the total orders list is used to predict items
--- cart_pred.py
+++ cart_pred.py
@@ -33,46 +33,61 @@ def read_mongo(db, collection, query={}, host='localhost', port=27017, username=
 
     return df
 
+def apriori_algo(mining_dataset, min_support):
+    """ Read the dataframe and returns the frequent item set"""
+
+    transactions = []
+    row, col = mining_dataset.shape
+
+    # converting the dataset into a list of lists for use in apriori method
+    for i in range(0, row):
+        transactions.append([str(mining_dataset.values[i, j]) for j in range(0, col)])
+
+    # removing the null items in the transactions
+    for i in range(0, row):
+        transactions[i] = list(filter(None, transactions[i]))
+
+    association_rules = apriori(transactions, min_support=min_support, min_confidence=0.2, min_lift=3, min_length=2)
+    association_results = list(association_rules)
+
+    req_rules = association_results[:3]
+    return req_rules
+
 
 if __name__ == '__main__':
     dataset = read_mongo('test', 'orders', {})
 
-
+    #taking user id as input
     print("Enter the user ID (1-100):")
     id = input()
-
-    print("id is :", id)
     id = int(id)
 
+    if id > 50:
+        print("User ID not found, returning the most frequent items bought by customers")
+
+    # extracting orders by the user from the whole orders dataset
     user_orders = dataset.loc[dataset['User ID'] == id]
     user_orders = user_orders.drop('User ID', 1)
     row, col = user_orders.shape
-    #print(user_orders)
 
-
+    #dropping the 1st column which is the user id
     dataset = dataset.drop('User ID', 1)
 
-
-    #print(dataset)
-
+    #if no of ordersby user is more than 50, use the user_orders, else use the whole dataset
     if row > 50:
         mining_dataset = user_orders
-        #print("inside if", row, col)
+        user_orders_row, user_orders_col = user_orders.shape
+        support = 10/user_orders_row
+        min_support = support
 
     else:
         mining_dataset = dataset
+        min_support = 0.0045
 
-    transactions = []
-    row, col = dataset.shape
-    for i in range(0, row):
-        #print("here")
-        transactions.append([str(dataset.values[i,j]) for j in range(0, col)])
-    print(transactions)
+    req_rules = apriori_algo(mining_dataset, min_support)
 
-    association_rules = apriori(transactions, min_support=0.0045, min_confidence=0.2, min_lift=3, min_length=2)
-    association_results = list(association_rules)
-    #print("here1")
-    req_rules = association_results[:3]
+    if len(req_rules) == 0:
+        req_rules = apriori_algo(dataset, 0.0045)
 
     for item in req_rules:
         pair = item[0]
